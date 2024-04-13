429c703c9ee2eebe2342b9eb5bc5dad2820621bc
Colt <austincgause@gmail.com>
2024-04-13 12:06:22 -0400

    Docs: added comments for enhanced code understanding

--- counter-app/src/main.rs
+++ counter-app/src/main.rs
@@ -9,6 +9,9 @@ use ratatui::{
 
 mod tui;
 
+// Main method with initializes terminal with help of helper module, begins the main loop with the
+// App's run method, and then calls the tui restore helper method to effectively shutdown the
+// application and return terminal control back to the user and then app_result is returned
 fn main() -> io::Result<()> {
     let mut terminal = tui::init()?;
     let app_result = App::default().run(&mut terminal);
@@ -16,14 +19,18 @@ fn main() -> io::Result<()> {
     app_result
 }
 
+// An App struct that defines the basic state that the application needs: a counter to keep track
+// of the number, and an exit bool that indicates if the application should exit
 #[derive(Debug, Default)]
 pub struct App {
     counter: u8,
     exit: bool,
 }
 
+// An impl of the app struct that defines various methods needed for the app to function
 impl App {
-    /// runs the application's main loop until the user quits
+
+    // Runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
@@ -32,13 +39,15 @@ impl App {
         Ok(())
     }
 
+    // Renders the frames with a set size: the entire terminal window
     fn render_frame(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.size());
     }
 
-    /// updates the application's state based on user input
+    // Updates the application's state based on user input
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
+
             // it's important to check that the event is a key press event as
             // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
@@ -49,6 +58,7 @@ impl App {
         Ok(())
     }
 
+    // Handles the various key events that the application supports
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
@@ -58,20 +68,28 @@ impl App {
         }
     }
 
+    // Changes the exit flag to indicate that the application should exit
     fn exit(&mut self) {
         self.exit = true;
     }
 
+    // Incrememnts the counter
     fn increment_counter(&mut self) {
         self.counter += 1;
     }
 
+    // Decrements the counter
     fn decrement_counter(&mut self) {
         self.counter -= 1;
     }
 }
 
+// An impl of Widget for the App class, this allows all of the rendering to be done seperately from
+// the main App impl, which allows the app itself to be called by the render_widget, simplifying a
+// significant portion of code
 impl Widget for &App {
+
+    // Renders the various UI elements
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Title::from(" Counter App Tutorial ".bold());
         let instructions = Title::from(Line::from(vec![
@@ -104,6 +122,7 @@ impl Widget for &App {
     }
 }
 
+// Tests
 #[cfg(test)]
 mod tests {
     use super::*;
